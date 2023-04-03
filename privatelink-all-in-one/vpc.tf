3f90c525b89b5fc3228e2a7c50bb2be556da3df9
Antonio Irizarry <55394816+airizarryDB@users.noreply.github.com>
2023-04-03 12:14:41 -0400

    Update vpc.tf

--- privatelink-all-in-one/vpc.tf
+++ privatelink-all-in-one/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.70.0"
+  version = "3.19.0"
 
   name                  = var.project_name
   cidr                  = var.cidr_block_private
@@ -69,7 +69,7 @@ module "vpc" {
 
 module "vpc_endpoints" {
   source             = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-  version            = "3.2.0"
+  version            = "3.19.0"
   vpc_id             = module.vpc.vpc_id
   security_group_ids = [module.vpc.default_security_group_id]
   endpoints = {
