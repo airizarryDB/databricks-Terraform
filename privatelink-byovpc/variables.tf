00abd9cc8355cf8217b3744176399caf255e8cba
Antonio Irizarry <55394816+airizarryDB@users.noreply.github.com>
2024-01-10 12:26:37 -0500

    Update variables.tf

--- privatelink-byovpc/variables.tf
+++ privatelink-byovpc/variables.tf
@@ -82,9 +82,20 @@ variable "project_name" {
   description = "Name that will be used in the workspace URL"
 }
 
-locals {
+variable "resource_prefix" {
   description = "Prefix used for root bucket name"
-  prefix = lower("test") // Must be changed
+  validation {
+    condition     = (can(regex("^[a-z-]+$", var.resource_prefix)))
+    error_message = "Invalid variable name, make sure it is all lower case"
+  }
 }
 
+variable "ucname" {
+  description = "URL compliant name for Unity Catalog Metastore"
+  type = string
+}
 
+variable "aws_account_id" {
+  description = "AWS Account ID"
+  type = string
+}
